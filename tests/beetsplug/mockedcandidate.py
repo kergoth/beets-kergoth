e4a28d9996eb6c93a3d66653293c7c72522b4096
Christopher Larson <kergoth@gmail.com>
2019-09-16 11:17:44 -0700

    Fix unicode python version issue in mockedcandidate
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- tests/beetsplug/mockedcandidate.py
+++ tests/beetsplug/mockedcandidate.py
@@ -18,22 +18,22 @@ class MockedCandidatePlugin(plugins.BeetsPlugin):
         return dist
 
     def get_track(self, artist=None):
-        return TrackInfo('A Track', 'http://foo', length=1, artist=artist or 'An Artist',
-                         artist_id=1, data_source='mocked_candidate',
-                         media='Digital Media', data_url='http://foo')
+        return TrackInfo(u'A Track', u'http://foo', length=1, artist=artist or u'An Artist',
+                         artist_id=1, data_source=u'mocked_candidate',
+                         media=u'Digital Media', data_url=u'http://foo')
 
     def get_album(self, artist=None, album=None):
         tracks = [self.get_track(artist)]
-        return AlbumInfo(album or 'An Album', 'mocked', artist or 'An Artist', 'http://foo', tracks,
+        return AlbumInfo(album or u'An Album', u'mocked', artist or u'An Artist', u'http://foo', tracks,
                          year=2019, month=1,
-                         day=1, country='XW', media='Digital Media',
-                         data_source='mocked_candidate', data_url='http://invalid')
+                         day=1, country=u'XW', media=u'Digital Media',
+                         data_source=u'mocked_candidate', data_url=u'http://invalid')
 
     def candidates(self, items, artist, album, va_likely):
         return [self.get_album(artist, album)]
 
     def album_for_id(self, album_id):
-        if album_id == 'mocked':
+        if album_id == u'mocked':
             return self.get_album()
 
     def item_candidates(self, item, artist, album):
