fa0b8a6473d0d05126851c8bb54ee91a361fb131
Christopher Larson <kergoth@gmail.com>
2019-09-26 10:30:10 -0700

    Rework queryalbum to convert the album query to an item query
    The downside is it currently isn't using an inner join, so is using
    multiple queries.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- beetsplug/queryalbum.py
+++ beetsplug/queryalbum.py
@@ -1,28 +1,46 @@
 """Query items whose album matches a sub-query.
 
 Ex. `beet ls album_has:albumtotal:1`
+
+FIXME: invalidate the cache as needed, and ideally rework to use a joined fetch
+with an inner join query based on the matching albums, if possible.
 """
 
 from __future__ import absolute_import, division, print_function
 
-from beets.dbcore import Query
+from beets import config
+from beets.dbcore import Query, MatchQuery, OrQuery
 from beets.library import Album, parse_query_string
 from beets.plugins import BeetsPlugin
+from beets.ui import _open_library
 
 
 class AlbumQuery(Query):
     """Query items whose album matches a sub-query."""
-    def __init__(self, query):
-        self.query_string = query
-        self.query, _ = parse_query_string(self.query_string, Album)
+    _cached_album_results = {}
+
+    def __init__(self, query_string):
+        self.query_string = query_string
+        if self.query_string in self._cached_album_results:
+            self.albums = self._cached_album_results[self.query_string]
+        else:
+            query, _ = parse_query_string(query_string, Album)
+            self.albums = self.lib.albums(query)
+        self.item_query = OrQuery([MatchQuery('album_id', album.id) for album in self.albums])
+
+    def clause(self):
+        return self.item_query.clause()
 
     def match(self, item):
-        album = item.get_album()
-        return self.query.match(album)
+        return self.item_query.match()
 
 
 class QueryAlbumPlugin(BeetsPlugin):
     def __init__(self):
         super(QueryAlbumPlugin, self).__init__()
 
-        self.item_queries = {'album_has': AlbumQuery}
+        class RealAlbumQuery(AlbumQuery):
+            lib = _open_library(config)
+            log = self._log
+
+        self.item_queries = {'album_has': RealAlbumQuery}
