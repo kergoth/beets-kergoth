a15cc5d0b75d7a3fff5776049ac0ec32499dd1df
Christopher Larson <kergoth@gmail.com>
2021-10-25 13:22:07 -0700

    inconsistentalbumtracks: pare down the output
    It now notices if a given field is inconsistent across all tracks and shows
    a single message for this case for that field, to avoid spamming the user.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- beetsplug/inconsistentalbumtracks.py
+++ beetsplug/inconsistentalbumtracks.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import, division, print_function
 
+from collections import defaultdict
+
 from beets import ui
 from beets.library import Album
 from beets.plugins import BeetsPlugin
@@ -29,7 +31,18 @@ class InconsistentAlbumTracks(BeetsPlugin):
     def command(self, lib, opts, args):
         query = decargs(args)
         for album in lib.albums(query):
-            for item in album.items():
+            inconsistent_fields = defaultdict(list)
+            album_items = album.items()
+
+            for item in album_items:
                 for field in self.album_fields:
                     if item[field] != album[field]:
+                        inconsistent_fields[field].append(item)
+
+            for field in sorted(inconsistent_fields):
+                items = inconsistent_fields[field]
+                if len(items) == len(album_items):
+                    print_(u'{}: field `{}` has album value `{}` but all track values are `{}`'.format(album, field, album[field], items[0][field]))
+                else:
+                    for item in items:
                         print_(u'{}: field `{}` has value `{}` but album value is `{}`'.format(item, field, item[field], album[field]))
