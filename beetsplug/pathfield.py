affec150c2099de38738ef84aa8bc934166a3b86
Christopher Larson <kergoth@gmail.com>
2021-10-28 19:40:38 -0700

    Revert "pathfield: use the decorators"
    This reverts commit 44778109860d2ccb038c1fedc5b534f528f39769.

--- beetsplug/pathfield.py
+++ beetsplug/pathfield.py
@@ -3,32 +3,39 @@
 - path: join the arguments with a null (\0)
 - pathfield: replace a null (\0) with the path separator
 
-With the former, we can store a path with \0 separators in a saved format, such
-as with the savedformats plugin. With the latter, we can make use of such a path
-in a path format, whether provided by that plugin or the inline plugin.
+With the former, we can store a path with \0 separators in a saved format, i.e.
+with the savedformats plugin. With the latter, we can make use of such a path in
+a path format, whether provided by that plugin or the inline plugin.
 
 Example:
 
-  item_fields: some_path: '\0'.join('Music', 'Subdir1', genre)
+  item_fields:
+    some_path: '\0'.join('Music', 'Subdir1', genre)
 
-  item_formats: some_other_path: '%path{Music,Subdir2,$composer}'
+  item_formats:
+    some_other_path: '%path{Music,Subdir2,$composer}'
 
-  paths: comp:1: %pathfield{$some_other_path}/%$artist - $title default:
-    %pathfield{$some_path}/%$artist - $title
+  paths:
+    comp:1: %pathfield{$some_other_path}/%$artist - $title
+    default: %pathfield{$some_path}/%$artist - $title
 """
 
 from __future__ import division, absolute_import, print_function
 
+from beets import config
 from beets.plugins import BeetsPlugin
+from beets.library import Album, Item
 
 
 class PathfieldPlugin(BeetsPlugin):
-    """Add template functions for working with paths via fields."""
+    def __init__(self):
+        super().__init__()
 
-    @BeetsPlugin.template_func("path")
+        self.template_funcs['path'] = self.tmpl_path
+        self.template_funcs['pathfield'] = self.tmpl_pathfield
+
     def tmpl_path(self, *p):
-        return "\0".join(p)
+        return '\0'.join(p)
 
-    @BeetsPlugin.template_func("pathfield")
     def tmpl_pathfield(self, path):
-        return path.replace("\0", "/")
+        return path.replace('\0', '/')
