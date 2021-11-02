6e54fcb7ac5b0b8ff8241ee0968348afc2a09c94
Christopher Larson <kergoth@gmail.com>
2021-11-02 15:38:16 -0700

    savedformats: cache field lookups
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- beetsplug/savedformats.py
+++ beetsplug/savedformats.py
@@ -1,10 +1,13 @@
 """Define saved format strings by storing them in fields."""
 
 from __future__ import division, absolute_import, print_function
+from collections import defaultdict
 
 from beets import config
+from beets.library import Album
 from beets.plugins import BeetsPlugin
-from beets.library import Album, Item
+from beets.util import functemplate
+from beets.dbcore import types
 
 
 class SavedFormatsPlugin(BeetsPlugin):
@@ -16,29 +19,55 @@ class SavedFormatsPlugin(BeetsPlugin):
     def __init__(self):
         super(SavedFormatsPlugin, self).__init__()
 
-        config.add({
-            'item_formats': {},
-            'album_formats': {},
-        })
+        config.add(
+            {
+                "item_formats": {},
+                "album_formats": {},
+            }
+        )
+        self.cache = defaultdict(lambda: defaultdict(dict))
+        self.register_listener("database_change", self.database_change)
+        self.register_listener("item_removed", self.item_removed)
         self.set_template_fields()
 
+    def database_change(self, _, item):
+        self.dirty_item(item)
+        if isinstance(item, Album):
+            for item in item.items():
+                self.dirty_item(item)
+
+    def item_removed(self, item):
+        self.dirty_item(item)
+
+    def dirty_item(self, item):
+        cache = self.cache[item.__class__]
+        if item.id and item.id in cache:
+            self._log.warning(f"Invalidating cache for {item}")
+            del cache[item.id]
+
     def set_template_fields(self):
-        for field, template in config['item_formats'].items():
-            self._log.debug('adding item field {}', field)
+        for name, cfgname, fieldsname in (
+            ("item", "item_formats", "template_fields"),
+            ("album", "album_formats", "album_template_fields"),
+        ):
+            for field, template in config[cfgname].items():
+                self._log.debug(f"adding {name} field {field}")
 
-            template = template.as_str()
+                templatestr = template.as_str()
+                template = functemplate.template(templatestr)
 
-            def apply_item(item, field=field, template=template):
-                value = item.evaluate_template(template)
-                return item._type(field).parse(value)
-            self.template_fields[field] = apply_item
+                def apply_item(item, field=field, template=template):
+                    # I would like to pre-determine the field types up front,
+                    # but there's no event that's fired after the plugin queries
+                    # and fields are merged, so we wait until first use instead.
+                    if item.id in self.cache:
+                        if field in self.cache[item.id]:
+                            return self.cache[item.id][field]
+                    field_type = item._type(field)
+                    value = self.cache[item.id][field] = field_type.parse(
+                        item.evaluate_template(template)
+                    )
+                    return value
 
-        for field, template in config['album_formats'].items():
-            self._log.debug('adding album field {}', field)
-
-            template = template.as_str()
-
-            def apply_album(album, field=field, template=template):
-                value = album.evaluate_template(template)
-                return album._type(field).parse(value)
-            self.album_template_fields[field] = apply_album
+                fields = getattr(self, fieldsname)
+                fields[field] = apply_item
