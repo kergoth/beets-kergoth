d61eb9bf2d2d2e9e3bd6cfa42d1ee4d32612e271
fortysix2ahead <fortysix2ahead@gmail.com>
2024-08-19 15:13:32 +0200

    Added configurability

--- beetsplug/tablelist.py
+++ beetsplug/tablelist.py
@@ -1,6 +1,7 @@
 
 from optparse import Values
 
+from confuse import Configuration
 from rich import box
 from rich.console import Console
 from rich.table import Table
@@ -10,25 +11,50 @@ from beets.library import Library
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand, decargs
 
-from shlex import split
+# from shlex import split
 
 console = Console()
 
 default_item_fmt = '$id $track $title $length $artist $album $year "$disc/$disctotal"'
 default_album_fmt = '$id $albumartist $album $year $genre $mb_albumid'
 
-def tablelist( lib: Library, opts: Values, args ):
+def cmd_tablelist( lib: Library, opts: Values, args, config ):
     query = decargs( args )
     results = lib.albums( query=query ) if opts.album else lib.items( query=query )
-    table = make_table( results, True ) if opts.album else make_table( results, False )
+    use_album_format = True if opts.album else False
+
+    table = make_table( results, config, use_album_format )
     console.print( table )
 
-def make_table( results: Results, use_album_format: bool = False ) -> Table:
-    table = Table( box=box.MINIMAL, show_header=False )
-    field_list = split( default_album_fmt ) if use_album_format else split( default_item_fmt )
+def make_style( config: Configuration ) -> dict:
+    cfg = {}
+
+    try:
+        cfg['box'] = getattr( box, config['style']['box'].get( str ).upper() )
+    except AttributeError:
+        print( 'warning: invalid box style' )
+        cfg['box'] = box.MINIMAL
+
+    cfg['show_header'] = config['style']['show_header'].get( bool )
+    cfg['row_styles'] = ['', 'dim']
+
+    return cfg
+
+def make_table( results: Results, config: Configuration, use_album_format: bool = False ) -> Table:
+    table_style = make_style( config )
+    table = Table( **table_style )
+
+    fields = config['album_columns'].get( str ) if use_album_format else config['item_columns'].get( str )
+    fields = [ f for f in fields.split() ]
+    fields_tmpl = [ '${}'.format( f ) for f in fields ]
+
+    if table_style['show_header']:
+        [ table.add_column( c ) for c in fields ]
+
     for r in results:
-        evaluated_field_list = [ r.evaluate_template( tmpl ) for tmpl in field_list ]
+        evaluated_field_list = [ r.evaluate_template( tmpl ) for tmpl in fields_tmpl ]
         table.add_row( *evaluated_field_list )
+
     return table
 
 class TableListPlugin( BeetsPlugin ):
@@ -36,8 +62,20 @@ class TableListPlugin( BeetsPlugin ):
     def __init__(self):
         super(TableListPlugin, self).__init__()
 
+        # default config
+        self.config.add(
+            {
+                'album_columns': 'albumartist album year',
+                'item_columns': 'track title artist album year',
+                'style': {
+                    'box': 'minimal',
+                    'show_header': True,
+                }
+            }
+        )
+
     def commands(self):
         cmd = Subcommand( 'tablelist', help='lists items in a table', aliases='tls' )
         cmd.parser.add_all_common_options()
-        cmd.func = tablelist
+        cmd.func = lambda lib, opts, args: cmd_tablelist(lib, opts, args, self.config)
         return [cmd]
