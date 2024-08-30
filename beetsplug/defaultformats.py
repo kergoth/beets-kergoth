759c54046c4b54d52855a908264cd18daa3c7b9e
Christopher Larson <kergoth@gmail.com>
2024-08-29 17:17:59 -0700

    defaultformats: add format_album to item and format_item to album
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- beetsplug/defaultformats.py
+++ beetsplug/defaultformats.py
@@ -5,7 +5,7 @@ For example, this enables one to easily adjust the existing format to append an 
     beet ls -f '$format_item [$existing/$albumtotal]'
 """
 
-from __future__ import division, absolute_import, print_function
+from __future__ import absolute_import, division, print_function
 
 from beets import config
 from beets.plugins import BeetsPlugin
@@ -20,12 +20,18 @@ class DefaultFormatsPlugin(BeetsPlugin):
         self.format_item = config['format_item'].get()
         self.format_album = config['format_album'].get()
 
-        self._log.debug('adding item field format_item')
         def format_item(item):
             return item.evaluate_template(self.format_item)
-        self.template_fields['format_item'] = format_item
 
-        self._log.debug('adding album field format_album')
         def format_album(album):
             return album.evaluate_template(self.format_album)
+
+        self._log.debug('adding item field format_item')
+        self.template_fields['format_item'] = format_item
+        self._log.debug("adding item field format_album (for the item's album)")
+        self.template_fields['format_album'] = format_album
+
+        self._log.debug('adding album field format_album')
         self.album_template_fields['format_album'] = format_album
+        self._log.debug('adding album field format_item (alias for format_album)')
+        self.album_template_fields['format_item'] = format_album
