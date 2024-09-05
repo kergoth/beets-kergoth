0246d8f1e2293d810a8450bd7bad0553723f6e69
Christopher Larson <kergoth@gmail.com>
2024-09-04 18:25:13 -0700

    importinspect: use text_diff_added for new fields
    This is more appropriate than text_highlight.
    
    Signed-off-by: Christopher Larson <kergoth@gmail.com>

--- beetsplug/importinspect.py
+++ beetsplug/importinspect.py
@@ -173,7 +173,7 @@ def show_model_changes(new, old=None, fields=None, always=False):
             continue
 
         changes.append(
-            f"  {field}: {colorize('text_highlight', new_fmt[field])}"
+            f"  {field}: {colorize('text_diff_added', new_fmt[field])}"
         )
 
     # Print changes.
