99a77e3921c4f5a30b55858f2935b430de353aa3
fortysix2ahead <fortysix2ahead@gmail.com>
2025-01-30 14:14:13 +0100

    Added --count option

--- beetsplug/genres.py
+++ beetsplug/genres.py
@@ -7,12 +7,19 @@ from beets.ui import Subcommand, decargs
 def cmd_genres(lib: Library, opts: Values, args, config):
     query = decargs(args)
     results = lib.albums(query) if opts.album else lib.items(query)
+    items = 'albums' if opts.album else 'tracks'
 
-    genres = set()
-    [genres.add(r.genre) for r in results]
+    genres = {}
+    for r in results:
+        if r.genre in genres.keys():
+            genres[r.genre] += 1
+        else:
+            genres[r.genre] = 1
 
-    for g in sorted(genres):
-        print( '<empty genre>' ) if g == '' else print(g)
+    for g in sorted(genres.keys()):
+        genre = '<empty>' if g in ['',None] else g
+        s = f'{genre} ({genres.get(g)} {items})' if opts.count else genre
+        print(s)
 
 class GenresPlugin(BeetsPlugin):
     def __init__(self):
@@ -21,5 +28,6 @@ class GenresPlugin(BeetsPlugin):
     def commands(self):
         cmd = Subcommand("genres", help="prints a consolidated list of genres of the result of a query" )
         cmd.parser.add_album_option()
+        cmd.parser.add_option( "-c", dest="count", action="store_true", help="counts items" )
         cmd.func = lambda lib, opts, args: cmd_genres(lib, opts, args, self.config)
         return [cmd]
